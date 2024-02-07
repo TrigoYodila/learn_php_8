48eda3012d6e7253b560a2b7edada9bb7e519db1
Trigo <trigoyodila1996@gmail.com>
2024-02-07 14:18:58 +0100

    create fns to format amount and date

--- views/transactions.php
+++ views/transactions.php
@@ -37,10 +37,10 @@
                 <?php if(! empty($transactions)): ?>
                     <?php foreach($transactions as $transaction): ?>
                         <tr>
-                            <td><?= $transaction['date']  ?></td>
+                            <td><?= formatDate($transaction['date'])  ?></td>
                             <td><?= $transaction['checkNumber']  ?></td>
                             <td><?= $transaction['description']  ?></td>
-                            <td><?= $transaction['amount']  ?></td>
+                            <td><?= formatDollarAmount($transaction['amount'])  ?></td>
                         </tr>
                     <?php endforeach ?>
                 <?php endif ?>
@@ -48,15 +48,15 @@
             <tfoot>
                 <tr>
                     <th colspan="3">Total Income:</th>
-                    <td><?= $totals['totalIncome'] ?? 0 ?></td>
+                    <td><?= formatDollarAmount($totals['totalIncome']) ?? 0 ?></td>
                 </tr>
                 <tr>
                     <th colspan="3">Total Expense:</th>
-                    <td><?= $totals['totalExpense'] ?? 0 ?></td>
+                    <td><?= formatDollarAmount($totals['totalExpense']) ?? 0 ?></td>
                 </tr>
                 <tr>
                     <th colspan="3">Net Total:</th>
-                    <td><?= $totals['netTotal'] ?? 0 ?></td>
+                    <td><?= formatDollarAmount($totals['netTotal']) ?? 0 ?></td>
                 </tr>
             </tfoot>
         </table>
