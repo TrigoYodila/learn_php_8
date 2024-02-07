27a20fac8db1eda01a9e47122f7519599b7ff2fa
Trigo <trigoyodila1996@gmail.com>
2024-02-07 12:09:49 +0100

    add function for reading all files in transactions_files

--- app/App.php
+++ app/App.php
@@ -1,3 +1,21 @@
 <?php
 
 declare(strict_types = 1);
+
+function getTransactionFiles():array{
+    $files = [];
+
+    //parcours les fichiers du chemin specifié
+    foreach (scandir(FILES_PATH) as $file) {
+        
+        //ne fait rien, continue si l'elt est un dossier
+        if(is_dir($file)){
+            continue;
+        }
+
+        //ajoute dans le tableau si c'est un fichier
+        $files[] = $file;
+    }
+
+    return $files;
+}
--- public/index.php
+++ public/index.php
@@ -7,3 +7,9 @@ $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
+
+//import app.php file
+require APP_PATH . "App.php";
+
+//call fn defined to App.php file
+getTransactionFiles();
